caabdac34c9c1517891d43089d1f7118efb61116
Adharsh <adharshkrish@outlook.com>
2020-04-03 21:37:57 +0530

    permenant delete and restore

---
+++ restore.php
@@ -0,0 +1,17 @@
+<?php
+    $con=mysqli_connect("localhost","root","","notes");
+    $user = $_GET['user'];
+    $id = $_GET['id'];
+    $sql = "update notes set deleted=0 where id=".$id;
+    // $sql = "delete from notes where id=".$id;
+    $result = mysqli_query($con,$sql);
+    if($result){
+        echo 'done';
+        // header("Location:http://localhost:8080/wt/home.jsp?user=".$user);
+        // $_SESSION["uname"] = $uname;
+    }else{
+        echo 'failed';
+    }
+
+
+?>
